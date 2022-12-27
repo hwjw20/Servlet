7fe602a982cea77b7c0eec68d1f0fe5f4f6c9b0d
303-01\user <hwjw666@naver.com>
2022-12-27 21:47:09 +0900

    [황지원] - 연습문제 10번 문제풀이

--- src/main/java/com/jiwon/servlet/servlet/test/Test10Controller.java
+++ src/main/java/com/jiwon/servlet/servlet/test/Test10Controller.java
@@ -33,15 +33,28 @@ public class Test10Controller extends HttpServlet{
 		// id가 일치하지 않는 경우 id가 일치하지 않습니다. 를 출력하세요.
 		// password가 일치하지 않는 경우 password가 일치하지 않습니다. 를 출력하세요.
 		// 일치하는 경우 아래와 같이 출력하세요. (HTML 화면)
-		if(!userMap.get("id").equals(id)) {
-			out.println("아이디가 일치하지 않습니다.");
+//		if(!userMap.get("id").equals(id)) {
+//			out.println("아이디가 일치하지 않습니다.");
+//		} else {
+//			if(!userMap.get("password").equals(password)) {
+//				out.println("비밀번호가 일치하지 않습니다.");
+//			} else {
+//				out.println("<html><head></head><body><h1>" + userMap.get("name") + "님 환영합니다.</h1></body></html>");
+//			}
+//		}
+		// 문제풀이
+		
+		out.println("<html><head><title>로그인</title></head><body>");
+		
+		if(!id.equals(userMap.get("id"))) {
+			out.println("<h2>아이디가 일치하지 않습니다.</h2>");
+		} else if(!password.equals(userMap.get("password"))) {
+			out.println("<h2>비밀번호가 일치하지 않습니다.</h2>");
 		} else {
-			if(!userMap.get("password").equals(password)) {
-				out.println("비밀번호가 일치하지 않습니다.");
-			} else {
-				out.println("<html><head></head><body><h1>" + userMap.get("name") + "님 환영합니다.</h1></body></html>");
-			}
+			out.println("<h1>" + userMap.get("name") + "님 환영합니다.");
 		}
+		
+		out.println("</body></html>");
 		
 	}
 	
