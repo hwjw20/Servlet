39c39e97a260f1ee85307b8ad367a2510603d9c5
303-01\user <hwjw666@naver.com>
2022-12-27 21:19:45 +0900

    [황지원] - 연습문제 10번 추가

---
+++ src/main/java/com/jiwon/servlet/servlet/test/Test10Controller.java
@@ -0,0 +1,50 @@
+package com.jiwon.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test10")
+public class Test10Controller extends HttpServlet{
+	
+	private final Map<String, String> userMap =  new HashMap<String, String>() {
+		{
+			put("id", "hagulu");
+			put("password", "asdf");
+			put("name", "김인규");
+		}
+	};
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		// id 와 password 를 입력 받고, submit 할 수 있는 html 문서를 작성하세요.
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		// id가 일치하지 않는 경우 id가 일치하지 않습니다. 를 출력하세요.
+		// password가 일치하지 않는 경우 password가 일치하지 않습니다. 를 출력하세요.
+		// 일치하는 경우 아래와 같이 출력하세요. (HTML 화면)
+		if(!userMap.get("id").equals(id)) {
+			out.println("아이디가 일치하지 않습니다.");
+		} else {
+			if(!userMap.get("password").equals(password)) {
+				out.println("비밀번호가 일치하지 않습니다.");
+			} else {
+				out.println("<html><head></head><body><h1>" + userMap.get("name") + "님 환영합니다.</h1></body></html>");
+			}
+		}
+		
+	}
+	
+	
+
+}
